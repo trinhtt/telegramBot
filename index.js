55db0707eb6bdf06760623ec47874140fb68f9cf
Thanh <thanhtuan.trinh@gmail.com>
2016-08-16 00:01:55 +0200

    Implemented get language for a user.

--- index.js
+++ index.js
@@ -41,3 +41,40 @@ bot.onText(/\/current/, function (msg) {
 
   bot.sendMessage(chatId, '/current', {reply_to_message_id: replyId});
 });
+
+bot.onText(/\/language/, function (msg) {
+  var chatId = msg.chat.id;
+  var userId = msg.from.id;
+  var replyId = msg.message_id;
+
+  if (dbInstance != null) {
+    dbClass.getPreferedLanguage(dbInstance, userId, function(err, callback) {
+      if (err == null) {
+        bot.sendMessage(chatId, 'Current language is ' + getFormattedLanguage(callback), {reply_to_message_id: replyId});
+      } else {
+        console.log(err);
+        bot.sendMessage(chatId, 'Couldnt find prefered language for user.', {reply_to_message_id: replyId});
+      }
+    });
+  }
+});
+
+/****************************************************/
+// Private methods
+/****************************************************/
+
+function getFormattedLanguage(language) {
+  switch (language) {
+    case Constants.databaseKeys.English.key:
+      return Constants.databaseKeys.English.display
+
+    case Constants.databaseKeys.SimplifiedChinese.key:
+      return Constants.databaseKeys.SimplifiedChinese.display
+
+    case Constants.databaseKeys.TraditionalChinese.key:
+      return Constants.databaseKeys.TraditionalChinese.display
+
+    default:
+      return Constants.databaseKeys.English.display
+  }
+}
