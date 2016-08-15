7f6ada7f29b28edf4d998a35493dd065f3f6d98e
Thanh <thanhtuan.trinh@gmail.com>
2016-08-16 00:15:01 +0200

    Implemented getCorrectURL from language

--- index.js
+++ index.js
@@ -110,3 +110,31 @@ function getFormattedLanguage(language) {
       return Constants.databaseKeys.English.display
   }
 }
+
+function getCorrectURL(language, type) {
+  switch (language) {
+    case Constants.databaseKeys.English.key:
+      if (type == Constants.topics.current) {
+        return Constants.webservices.English.rootURL+Constants.webservices.English.currentWeatherRSS
+      } else if (type == Constants.topics.warning) {
+        return Constants.webservices.English.rootURL+Constants.webservices.English.warningRSS
+      }
+
+    case Constants.databaseKeys.SimplifiedChinese.key:
+      if (type == Constants.topics.current) {
+        return Constants.webservices.SimplifiedChinese.rootURL+Constants.webservices.SimplifiedChinese.currentWeatherRSS
+      } else if (type == Constants.topics.warning) {
+        return Constants.webservices.SimplifiedChinese.rootURL+Constants.webservices.SimplifiedChinese.warningRSS
+      }
+
+    case Constants.databaseKeys.TraditionalChinese.key:
+      if (type == Constants.topics.current) {
+        return Constants.webservices.TraditionalChinese.rootURL+Constants.webservices.TraditionalChinese.currentWeatherRSS
+      } else if (type == Constants.topics.warning) {
+        return Constants.webservices.TraditionalChinese.rootURL+Constants.webservices.TraditionalChinese.warningRSS
+      }
+
+    default:
+      return Constants.webservices.English.rootURL+Constants.webservices.English.currentWeatherRSS
+  }
+}
