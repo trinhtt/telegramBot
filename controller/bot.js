180ea418ca2da723d2a99a3951cd8881d3cc06ff
Thanh <thanhtuan.trinh@gmail.com>
2016-08-20 22:50:29 +0200

    Separated bot operations into another file operations.js

--- controller/bot.js
+++ controller/bot.js
@@ -1,6 +1,7 @@
 var Constants = require('../utils/constants.js');
 var formatter = require('../utils/formatter.js');
 var weatherService = require('../services/weatherServices.js');
+var operations = require('./operations.js');
 var dbClass = require('./../db/database.js');
 var dbInstance = null;
 var bot = null;
@@ -16,8 +17,8 @@ function init(botClass,db){
     console.log('Hi my name is %s!', me.username);
   });
 
+  operations.init(bot, dbInstance);
   settingUpOntext(bot);
-
 }
 
 /****************************************************/
@@ -30,7 +31,7 @@ function settingUpOntext(bot){
     var userId = msg.from.id;
     var replyId = msg.message_id;
 
-    sendMessageWithFormattedLanguage(userId, chatId, Constants.topics.current, replyId, weatherService.getCurrentWeather);
+    operations.sendMessageWithFormattedLanguage(userId, chatId, Constants.topics.current, replyId, weatherService.getCurrentWeather);
   });
 
   bot.onText(/\/warning/, function (msg) {
@@ -38,7 +39,7 @@ function settingUpOntext(bot){
     var userId = msg.from.id;
     var replyId = msg.message_id;
 
-    sendMessageWithFormattedLanguage(userId, chatId, Constants.topics.warning, replyId, weatherService.getWarning);
+    operations.sendMessageWithFormattedLanguage(userId, chatId, Constants.topics.warning, replyId, weatherService.getWarning);
   });
 
   bot.onText(/\/topics/, function (msg) {
@@ -70,13 +71,7 @@ function settingUpOntext(bot){
         break;
     }
 
-    dbClass.writeSubscriber(dbInstance, collectionId, msg, function(err, result) {
-      if (err == null) {
-        bot.sendMessage(chatId, result, {reply_to_message_id: replyId});
-      } else {
-        bot.sendMessage(chatId, err, {reply_to_message_id: replyId});
-      }
-    });
+    operations.writeSubscriber(collectionId, msg, chatId, replyId);
   });
 
   bot.onText(/(\/unsubscribe current)|(\/unsubscribe warning)/, function (msg, match) {
@@ -97,13 +92,7 @@ function settingUpOntext(bot){
         break;
     }
 
-    dbClass.deleteSubscriber(dbInstance, collectionId, userId, function(err, result) {
-      if (err == null) {
-        bot.sendMessage(chatId, result, {reply_to_message_id: replyId});
-      } else {
-        bot.sendMessage(chatId, err, {reply_to_message_id: replyId});
-      }
-    });
+    operations.deleteSubscriber(collectionId, userId, chatId, replyId);
   });
 
   bot.onText(/\/language/, function (msg) {
@@ -111,16 +100,7 @@ function settingUpOntext(bot){
     var userId = msg.from.id;
     var replyId = msg.message_id;
 
-    if (dbInstance != null) {
-      dbClass.getPreferedLanguage(dbInstance, userId, function(err, callback) {
-        if (err == null) {
-          bot.sendMessage(chatId, 'Current language is ' + formatter.getFormattedLanguage(callback), {reply_to_message_id: replyId});
-        } else {
-          console.log(err);
-          bot.sendMessage(chatId, 'Couldnt find prefered language for user.', {reply_to_message_id: replyId});
-        }
-      });
-    }
+    operations.getPreferedLanguage(userId, chatId, replyId);
   });
 
   bot.onText(/\/(simplified)|(traditional)|(english)/, function (msg, match) {
@@ -146,43 +126,9 @@ function settingUpOntext(bot){
         lang = Constants.databaseKeys.English.key;
     }
 
-    dbClass.createNewUserPreferences(dbInstance, userId, lang, function(err, results) {
-      if (err == null) {
-        bot.sendMessage(chatId, 'Set new language: ' + lang, {reply_to_message_id: replyId});
-      } else {
-        bot.sendMessage(chatId, 'Couldnt change language.', {reply_to_message_id: replyId});
-      }
-    });
+    operations.createNewUserPreferences(userId, lang, chatId, replyId)
   });
 }
-
-/****************************************************/
-// Bot methods to send messages
-/****************************************************/
-
-function sendMessageWithFormattedLanguage(userId, chatId, type, replyId, getMethod, update) {
-  if (dbInstance != null) {
-    dbClass.getPreferedLanguage(dbInstance, userId, function(err, callback) {
-      if (err == null) {
-        var url = formatter.getCorrectURL(callback, type);
-        var resp2 = getMethod(url, function (result, err) {
-          if (err == null) {
-            if (update != null) {
-              result = 'UPDATE: \n\n' + result;
-            }
-            bot.sendMessage(chatId, result, {reply_to_message_id: replyId});
-          } else {
-            bot.sendMessage(chatId, err, {reply_to_message_id: replyId});
-          }
-        });
-      } else {
-        console.log(err);
-        bot.sendMessage(chatId, err, {reply_to_message_id: replyId});
-      }
-    });
-  }
-}
-
 
 /****************************************************/
 // Exporting Bot
---
+++ controller/operations.js
@@ -0,0 +1,96 @@
+var formatter = require('../utils/formatter.js');
+var dbClass = require('./../db/database.js');
+var dbInstance = null;
+var bot = null;
+
+/****************************************************/
+// Operations initialization
+/****************************************************/
+
+function init(botClass,db){
+  bot = botClass;
+  dbInstance = db;
+}
+
+/****************************************************/
+// Private methods
+/****************************************************/
+
+function sendMessageWithFormattedLanguage(userId, chatId, type, replyId, getMethod, update) {
+  if (dbInstance != null) {
+    dbClass.getPreferedLanguage(dbInstance, userId, function(err, callback) {
+      if (err == null) {
+        var url = formatter.getCorrectURL(callback, type);
+        var resp2 = getMethod(url, function (result, err) {
+          if (err == null) {
+            if (update != null) {
+              result = 'UPDATE: \n\n' + result;
+            }
+            bot.sendMessage(chatId, result, {reply_to_message_id: replyId});
+          } else {
+            bot.sendMessage(chatId, err, {reply_to_message_id: replyId});
+          }
+        });
+      } else {
+        console.log(err);
+        bot.sendMessage(chatId, err, {reply_to_message_id: replyId});
+      }
+    });
+  }
+}
+
+function writeSubscriber(collectionId, msg, chatId, replyId) {
+  dbClass.writeSubscriber(dbInstance, collectionId, msg, function(err, result) {
+    if (err == null) {
+      bot.sendMessage(chatId, result, {reply_to_message_id: replyId});
+    } else {
+      bot.sendMessage(chatId, err, {reply_to_message_id: replyId});
+    }
+  });
+}
+
+function deleteSubscriber(collectionId, userId, chatId, replyId) {
+  dbClass.deleteSubscriber(dbInstance, collectionId, userId, function(err, result) {
+    if (err == null) {
+      bot.sendMessage(chatId, result, {reply_to_message_id: replyId});
+    } else {
+      bot.sendMessage(chatId, err, {reply_to_message_id: replyId});
+    }
+  });
+}
+
+function getPreferedLanguage(userId, chatId, replyId) {
+  dbClass.getPreferedLanguage(dbInstance, userId, function(err, callback) {
+    if (err == null) {
+      bot.sendMessage(chatId, 'Current language is ' + formatter.getFormattedLanguage(callback), {reply_to_message_id: replyId});
+    } else {
+      console.log(err);
+      bot.sendMessage(chatId, 'Couldnt find prefered language for user.', {reply_to_message_id: replyId});
+    }
+  });
+}
+
+function createNewUserPreferences(userId, lang, chatId, replyId) {
+  dbClass.createNewUserPreferences(dbInstance, userId, lang, function(err, results) {
+    if (err == null) {
+      bot.sendMessage(chatId, 'Set new language: ' + lang, {reply_to_message_id: replyId});
+    } else {
+      bot.sendMessage(chatId, 'Couldnt change language.', {reply_to_message_id: replyId});
+    }
+  });
+}
+
+/****************************************************/
+// Exporting operations
+/****************************************************/
+var operations = {
+    init : init,
+    sendMessageWithFormattedLanguage: sendMessageWithFormattedLanguage,
+    writeSubscriber: writeSubscriber,
+    deleteSubscriber: deleteSubscriber,
+    getPreferedLanguage: getPreferedLanguage,
+    createNewUserPreferences: createNewUserPreferences
+};
+
+
+module.exports = operations;
